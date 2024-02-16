b158946d2e0ba55603fe393b30af3b23d144df7b
Antonio Burgov <119887445+AntonioBurgov@users.noreply.github.com>
2024-02-16 08:55:33 +0200

    Finishing the Cart Page

---
+++ frontend/src/app/components/partials/title/title.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrl: './title.component.css',
+})
+export class TitleComponent {
+  constructor() {}
+  @Input()
+  title!: string;
+
+  @Input()
+  margin? = '1rem 0 1rem 0.2rem';
+
+  @Input()
+  fontsize? = '1.7rem';
+}
